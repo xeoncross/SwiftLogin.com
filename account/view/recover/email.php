abb2c5245362a28b81bd02afee0b1a7febd991b4
Xeoncross <david@xeoncross.com>
2011-03-11 22:31:19 -0600

    Fixed recover email.

--- account/view/recover/email.php
+++ account/view/recover/email.php
@@ -1,7 +1,7 @@
 Forgotten Password Request<br />
 -------------------<br />
 <br />
-This is a one-time login email sent from emaillogin.net to allow you to recover your password.<br />
+This is a one-time login email sent from SwiftLogin.com to allow you to recover your password.<br />
 <br />
 -------------------<br />
 <br />
@@ -9,6 +9,8 @@ This is a one-time login email sent from emaillogin.net to allow you to recover 
 <br />
 -------------------<br />
 <br />
-You are receiving this message because you submitted a reset password request for this email address. If you did not try to login to emaillogin.net then just ignore this message. You may also report this to help@emaillogin.net.<br />
+You are receiving this message because you submitted a reset password request for this email address. 
+If you did not try to login to SwiftLogin.com then just ignore this message. 
+You may also report this to support@swiftlogin.com.<br />
 <br />
 IP Address: <?php print $ip_address; ?>
